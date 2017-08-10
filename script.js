a317fce4e194dcb5f4e82036949ccb72c206aca9
vahutson <iluchenko.vasiliy@gmail.com>
2017-08-10 10:42:57 +0300

    script processing

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-/**
- * Created by Vasyl on 07.08.2017.
- */
+// console.log('ok');
+//
+
+$.ajax({
+    url: "https://anypoint.mulesoft.com/apiconsoleproxy/api/v1/proxy/http://504080.com/api/v2/services/categories",
+    type: "GET",
+    headers: {'Authorization': 'ACCESS_TOKEN'},
+    done(function( data ) {
+        console.log('sucess');
+        console.log(data );
+    })
+    .error(function (data) {
+        console.log('error');
+        console.log(data );
+    });
+});
+
+
+$.get( "https://anypoint.mulesoft.com/apiconsoleproxy/api/v1/proxy/http://504080.com/api/v2/services/categories" )
+    .done(function( data ) {
+        console.log('sucess');
+        console.log(data );
+    })
+    .error(function (data) {
+        console.log('error');
+        console.log(data );
+    });
